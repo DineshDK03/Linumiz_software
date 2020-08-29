500363e89a98b4a9e21f4947bc0136cce5c28b5d
DineshDK03 <hubertdinesh@gmail.com>
2020-08-29 16:49:54 +0530

    Edited Find_elf.sh

--- Find_elf.sh
+++ Find_elf.sh
@@ -2,4 +2,9 @@
 
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
-du -ha --apparent-size -a $1/ |grep -i .elf |sort -n
+#du -ha --apparent-size -a $1/ | grep -i .elf |sort -n
+#find $1 -type f -exec "file {} | grep -Pi ': elf (32|64)-bit' > /dev/null" \; -print
+#find $1 -type f -exec du -ha --apparent-size {} \; | sort -n
+
+ls -shSr $(find $1 -type f -exec sh -c "file {} | grep -Pi ': elf (32|64)-bit' > /dev/null " \; -print)
+
